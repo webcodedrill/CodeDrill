b872fe2d622d2a95f20247db1ad01ab1211a6820
webcodedrill <webcodedrill@gmail.com>
2026-06-27 23:03:51 +0500

    Delete public directory

--- public/js/sidebar.js
+++
@@ -1,9 +0,0 @@
-let sidebarOpen = false;
-
-function toggleSidebar(val) {
-  sidebarOpen = val !== undefined ? val : !sidebarOpen;
-  const sb = document.querySelector('.sidebar');
-  const ov = document.querySelector('.sidebar-overlay');
-  if (sb) sb.classList.toggle('open', sidebarOpen);
-  if (ov) ov.classList.toggle('open', sidebarOpen);
-}
